9976b1deb5336439f45c2b3dbbfd2c8f99695c24
sachinNishalka <nishalkasachin@gmail.com>
2026-06-13 01:56:10 +0530

    completed basic nodes

--- src/node-details/EstimationNodeForm.jsx
+++ src/node-details/EstimationNodeForm.jsx
@@ -1,9 +1,18 @@
 import { useEdges, useReactFlow } from "@xyflow/react";
 import { useForm } from "react-hook-form";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import CustomSelect from "../components/CustomSelect";
+import { useGetFactorLibraries } from "./useGetFactorLibraries";
+import { useGetFactorMappings } from "./useGetFactorMappings";
+import { useGetEstimationFactors } from "./useGetEstimationFactors";
 
 export default function EstimationNodeForm({ nodeId }) {
+  const [factorLibarary, setFactorLibrary] = useState(null);
+  const [estimationFactor, setEstimationFactor] = useState(null);
+  const [filteredEstimationFactors, setFilteredEstimationFactors] =
+    useState(null);
+
   const { register, handleSubmit, setValue } = useForm();
   const { getNodes, updateNodeData } = useReactFlow();
 
@@ -17,7 +26,12 @@ export default function EstimationNodeForm({ nodeId }) {
     updateNodeData(nodeId, {
       meta: {
         inputs: connection?.source || "source",
-        formData: data,
+        formData: {
+          ...data,
+          factor_library: factorLibarary,
+          // TODO: shoudl change to factor mappings
+          estimation_factor: estimationFactor,
+        },
       },
     });
   };
@@ -25,7 +39,9 @@ export default function EstimationNodeForm({ nodeId }) {
   useEffect(() => {
     const node = nodes.find((node) => node.id === nodeId);
     const formData = node?.data?.meta?.formData;
-    setValue("factor_library", formData?.factor_library);
+
+    setFactorLibrary(factorLibarary || formData?.factor_library);
+    setEstimationFactor(estimationFactor || formData?.estimation_factor);
     setValue("estimation_factor", formData?.estimation_factor);
     setValue("estimation_value_column", formData?.estimation_value_column);
     setValue("estimation_unit_column", formData?.estimation_unit_column);
@@ -33,21 +49,60 @@ export default function EstimationNodeForm({ nodeId }) {
     setValue("output_unit_column", formData?.output_unit_column);
   }, [nodes]);
 
+  const { data: factorLibraries, isLoading: isFactorLibrariesLoading } =
+    useGetFactorLibraries();
+
+  const { data: estimationFactors, isLoading: isEstimationFactorsLoading } =
+    useGetEstimationFactors();
+
+  useEffect(() => {
+    if (factorLibarary != null) {
+      let filteredFactors = estimationFactors?.filter(
+        (factorLibrary) =>
+          factorLibrary.factorLibraryId === factorLibarary.value,
+      );
+      // TODO: this should be changed to factor mappings
+      setFilteredEstimationFactors(filteredFactors);
+    }
+  }, [factorLibarary]);
+
+  if (isFactorLibrariesLoading && isEstimationFactorsLoading)
+    return <div>Loading...</div>;
+
+  const optionsFactorLibraries = factorLibraries?.map((libarary) => ({
+    value: libarary.id,
+    label: libarary.name,
+  }));
+
+  const estimationFactorOptions = estimationFactors?.map((factor) => ({
+    value: factor.id,
+    label: factor.name,
+  }));
+
+  const filteredEstimationFactorOPtions = filteredEstimationFactors?.map(
+    (factor) => ({
+      value: factor.id,
+      label: factor.name,
+    }),
+  );
+
   return (
     <form className="flex flex-col gap-2 p-4" onSubmit={handleSubmit(onSubmit)}>
       <label>Factor Library</label>
-      <input
-        className="border"
-        type="text"
-        defaultValue="test"
-        {...register("factor_library")}
-      />
+      <CustomSelect
+        options={optionsFactorLibraries}
+        onItemChange={setFactorLibrary}
+        value={factorLibarary}
+      ></CustomSelect>
+
       <label>Estimation Factor</label>
-      <input
-        className="border"
-        type="text"
-        {...register("estimation_factor")}
-      />
+      <CustomSelect
+        options={filteredEstimationFactorOPtions || estimationFactorOptions}
+        isMulti={true}
+        onItemChange={setEstimationFactor}
+        value={estimationFactor}
+      ></CustomSelect>
+
       <label>Estimation Value Column</label>
       <input
         className="border"
